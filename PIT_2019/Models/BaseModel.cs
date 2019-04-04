aa4a00cf5c44bde889e5ae31fdf8e6800f7bf2df
AnnaMarija <annamarija.vrublevska@gmail.com>
2019-04-05 02:05:04 +0300

    ok

---
+++ PIT_2019/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PIT_2019.Models
+{
+    [Serializable]
+    public class JsonActionResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ PIT_2019/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PIT_2019.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Phome { get; set; }
+        public UserRights Rights { get; set; }
+    }
+
+    public enum UserRights
+    {
+        Seamstress = 1,
+        Administrator = 2
+    }
+}
